190e432c40ccbc995eb9f01117b64cd9cbed8fec
Erin Smith <erin.acumen@gmail.com>
2020-06-04 20:31:39 -0700

    prompts created and tested

--- Develop/app.js
+++ Develop/app.js
@@ -10,35 +10,96 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const questions = [
-    {
-      message: "What is your full name?",
-      name: "name"
-    },
-    {
-      message: "What is you email address",
-      name: "email"
-    },
-    {
-      message: "What is your position/job title at the company?",
-      name: "role"
-    },
-    {
-        message: "If you are a Manager, what is your office Number?",
-        name: "officeNumber",
-        when: answers => {
-            
+function promptManager ()
+{
+    const answers = inquirer.prompt([
+        {
+            message: "What is the name of the Team Manager?",
+            name: "name"
+        },
+        {
+            message: "What is the Team Manager's email address?",
+            name: "email"
+        }, {
+            message: "What is Team Manager's office number ?",
+            name: "officeNumber"
+        },
+    ]);
+    return answers;
+}
+
+function promptEmployeeInfo ()
+{
+    const answers = inquirer.prompt([
+        {
+            message: "Enter the full name of an employee",
+            name: "name"
+        },
+        {
+            message: "What is the employee's email address",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "Choose their role:",
+            name: "role",
+            choices: ["Engineer", "Intern"]
+        },
+
+        {
+            message: "What is the Github Username of the employee?",
+            name: "GithubUser",
+            when: (answers) =>
+            {
+                if (answers.role === "Engineer") {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            message: " What school does the Intern attend?",
+            name: "school",
+            when: (answers) =>
+            {
+                if (answers.role === "Intern") {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            message: "Do you have another employee to enter?",
+            name: "addEmployee"
         }
-      },
-    {
-      message: "If you are an Engineer, please type your Github Username:",
-      name: "GithubUser"
-    },
-    {
-      message: "If you are an Intern, What school do you attend?",
-      name: "school"
+    ]);
+    return answers;
+}
+
+async function main ()
+{
+    try {
+        const mgrInfo = await promptManager();
+        let staffArray = [];
+        while (true) {
+            const empInfo = await promptEmployeeInfo();
+            if (empInfo.addEmployee === false) {
+                break;
+            }
+        }
+
     }
-];
+    catch (err) {
+        throw err;
+    }
+}
+main();
+
 
 //ERIN 's plan:
 //1. get info from employees as data object like in readme: prompt user 
